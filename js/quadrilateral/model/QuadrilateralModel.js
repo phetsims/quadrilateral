964c4107f1eede66e263945701812262ff498815
Jesse <jesse.greenberg@colorado.edu>
2021-08-18 23:18:34 -0400

    initial commmit for quadrilateral model

--- js/quadrilateral/model/QuadrilateralModel.js
+++ js/quadrilateral/model/QuadrilateralModel.js
@@ -4,8 +4,16 @@
  * @author Jesse Greenberg (PhET Interactive Simulations)
  */
 
+import DerivedProperty from '../../../../axon/js/DerivedProperty.js';
+import NumberProperty from '../../../../axon/js/NumberProperty.js';
+import Bounds2 from '../../../../dot/js/Bounds2.js';
+import Range from '../../../../dot/js/Range.js';
+import Utils from '../../../../dot/js/Utils.js';
+import Vector2 from '../../../../dot/js/Vector2.js';
 import Tandem from '../../../../tandem/js/Tandem.js';
+import BooleanIO from '../../../../tandem/js/types/BooleanIO.js';
 import quadrilateral from '../../quadrilateral.js';
+import Vertex from './Vertex.js';
 
 class QuadrilateralModel {
 
@@ -14,7 +22,43 @@ class QuadrilateralModel {
    */
   constructor( tandem ) {
     assert && assert( tandem instanceof Tandem, 'invalid tandem' );
-    //TODO
+
+    // @public {Vertex}
+    this.vertex1 = new Vertex( new Vector2( -0.5, 0.5 ), new Bounds2( -1, 0, 0, 1 ), tandem.createTandem( 'vertex1' ) );
+    this.vertex2 = new Vertex( new Vector2( 0.5, 0.5 ), new Bounds2( 0, 0, 1, 1 ), tandem.createTandem( 'vertex2' ) );
+    this.vertex3 = new Vertex( new Vector2( 0.5, -0.5 ), new Bounds2( 0, -1, 1, 0 ), tandem.createTandem( 'vertex3' ) );
+    this.vertex4 = new Vertex( new Vector2( -0.5, -0.5 ), new Bounds2( -1, -1, 0, 0 ), tandem.createTandem( 'vertex4' ) );
+
+    // connect vertices to form the quadrilateral shape and initialize angles
+    this.vertex1.connectToOThers( this.vertex4, this.vertex2 );
+    this.vertex2.connectToOThers( this.vertex1, this.vertex3 );
+    this.vertex3.connectToOThers( this.vertex2, this.vertex4 );
+    this.vertex4.connectToOThers( this.vertex3, this.vertex1 );
+
+    // @public {NumberProperty} - A value that controls the threshold for equality when determining
+    // if the quadrilateral forms a parallelogram. Without a margin of error it would be exceedingly
+    // difficult to create a parallelogram shape. It is unclear whether this need to change, but
+    // it seems useful now to be able to easily change this value during development.
+    this.angleEqualityEpsilonProperty = new NumberProperty( 0.1, {
+      tandem: tandem.createTandem( 'angleEqualityEpsilonProperty' ),
+      range: new Range( 0.01, 0.3 )
+    } );
+
+    // @public {DerivedProperty.<boolean>} - A value that indicates that the quadrilateral is
+    // curently a parallelogram. We assume that the quad is a parallelogram if oposite angles
+    // are equal within a margin of error.
+    this.isParallelogramProperty = new DerivedProperty( [
+      this.vertex1.angleProperty,
+      this.vertex2.angleProperty,
+      this.vertex3.angleProperty,
+      this.vertex4.angleProperty,
+      this.angleEqualityEpsilonProperty
+    ], ( angle1, angle2, angle3, angle4, epsilon ) => {
+      return Utils.equalsEpsilon( angle1, angle3, epsilon ) && Utils.equalsEpsilon( angle2, angle4, epsilon );
+    }, {
+      tandem: tandem.createTandem( 'isParallelogramProperty' ),
+      phetioType: DerivedProperty.DerivedPropertyIO( BooleanIO )
+    } );
   }
 
   /**
@@ -22,7 +66,10 @@ class QuadrilateralModel {
    * @public
    */
   reset() {
-    //TODO
+    this.vertex1.reset();
+    this.vertex2.reset();
+    this.vertex3.reset();
+    this.vertex4.reset();
   }
 
   /**
---
+++ js/quadrilateral/model/Vertex.js
@@ -0,0 +1,78 @@
+// Copyright 2021, University of Colorado Boulder
+
+/**
+ * A model component for a Vertex of the Quadrilateral.
+ *
+ * @author Jesse Greenberg
+ */
+
+import DerivedProperty from '../../../../axon/js/DerivedProperty.js';
+import Vector2Property from '../../../../dot/js/Vector2Property.js';
+import NumberIO from '../../../../tandem/js/types/NumberIO.js';
+import quadrilateral from '../../quadrilateral.js';
+
+class Vertex {
+
+  /**
+   * @param {Vector2} initialPosition
+   * @param {Bounds2} bounds
+   * @param {Tandem} tandem
+   */
+  constructor( initialPosition, bounds, tandem ) {
+
+    // @public {Vector2Property} - the position of this vertex in model space
+    this.positionProperty = new Vector2Property( initialPosition, {
+      tandem: tandem.createTandem( 'positionProperty' ),
+      validBounds: bounds
+    } );
+
+    // @public {DerivedProperty.<number>|null} - the angle at this vertex of the quadrilateral, null until
+    // this vertex is connected to two others because we need three points to form the angle
+    this.angleProperty = null;
+
+    // @private {Tandem}
+    this.tandem = tandem;
+  }
+
+  /**
+   * Reset this vertex.
+   * @public
+   */
+  reset() {
+    this.positionProperty.reset();
+  }
+
+  /**
+   * Connect this vertex to two others to form an angle and sides of the quadrilateral.
+   * Uses the law of cosines to calculate the angle, assuming vertex positions like this:
+   *
+   *        thisVertex
+   *          /       \
+   *   sideA /         \ sideB
+   *        /           \
+   *     vertex1 ----- vertex2
+   *             sideC
+   *
+   * See https://en.wikipedia.org/wiki/Law_of_cosines
+   * @public
+   *
+   * @param {Vertex} vertex1
+   * @param {Vertex} vertex2
+   */
+  connectToOThers( vertex1, vertex2 ) {
+
+    this.angleProperty = new DerivedProperty( [ vertex1.positionProperty, this.positionProperty, vertex2.positionProperty ], ( vertex1Position, thisPosition, vertex2Position ) => {
+      const sideA = vertex1Position.distance( thisPosition );
+      const sideB = vertex2Position.distance( thisPosition );
+      const sideC = vertex2Position.distance( vertex1Position );
+
+      return Math.acos( ( ( sideA * sideA ) + ( sideB * sideB ) - ( sideC * sideC ) ) / ( 2 * sideA * sideB ) );
+    }, {
+      tandem: this.tandem.createTandem( 'angleProperty' ),
+      phetioType: DerivedProperty.DerivedPropertyIO( NumberIO )
+    } );
+  }
+}
+
+quadrilateral.register( 'Vertex', Vertex );
+export default Vertex;
