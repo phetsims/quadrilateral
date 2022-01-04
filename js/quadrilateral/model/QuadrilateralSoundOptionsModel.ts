9d9164d9620b59cd4bd4121733f8408b079b11d6
phet-dev <phetadmin@gmail.com>
2022-01-04 03:21:46 -0700

    update copyright dates from daily grunt work

--- js/quadrilateral/model/QuadrilateralSoundOptionsModel.ts
+++ js/quadrilateral/model/QuadrilateralSoundOptionsModel.ts
@@ -1,4 +1,4 @@
-// Copyright 2021, University of Colorado Boulder
+// Copyright 2021-2022, University of Colorado Boulder
 
 /**
  * A model for the sound designs that are being proposed for Quadrilateral. In active development and we are
