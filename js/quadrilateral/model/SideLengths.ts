50b15ed88f43b32b4861f101bb0b035870e72374
phet-dev <phetadmin@gmail.com>
2022-01-12 03:28:15 -0700

    update copyright dates from daily grunt work

--- js/quadrilateral/model/SideLengths.ts
+++ js/quadrilateral/model/SideLengths.ts
@@ -1,4 +1,4 @@
-// Copyright 2021, University of Colorado Boulder
+// Copyright 2021-2022, University of Colorado Boulder
 
 /**
  * A class that is a collection of side lengths at a snapshot in time. Used to compare how the collection of
