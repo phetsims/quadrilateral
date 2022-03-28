b5fe04b50b824c9cad6c1df2e5aef2f3b30cb22b
jbphet <john.blanco@colorado.edu>
2022-03-28 10:51:06 -0600

    typecast of property to avoid TS build issue, see https://github.com/phetsims/tambo/issues/160 and https://github.com/phetsims/axon/issues/382

--- js/quadrilateral/view/ShapeIdentificationSoundView.ts
+++ js/quadrilateral/view/ShapeIdentificationSoundView.ts
@@ -15,6 +15,7 @@ import shapeIdentificationWhenNotAParallelogram_mp3 from '../../../sounds/shapeI
 import allAnglesAreRightAngles_mp3 from '../../../sounds/allAnglesAreRightAngles_mp3.js';
 import allSideLengthsAreEqual_mp3 from '../../../sounds/allSideLengthsAreEqual_mp3.js';
 import QuadrilateralShapeModel from '../model/QuadrilateralShapeModel.js';
+import IProperty from '../../../../axon/js/IProperty.js';
 
 class ShapeIdentificationSoundView {
   private readonly shapeSoundClip: SoundClip;
@@ -25,7 +26,7 @@ class ShapeIdentificationSoundView {
 
     const soundClipOptions = {
       // don't play sounds while model reset is in progress
-      enableControlProperties: [ resetNotInProgressProperty ]
+      enableControlProperties: [ resetNotInProgressProperty as IProperty<boolean> ]
     };
 
     this.shapeSoundClip = new SoundClip( shapeIdentificationWhenNotAParallelogram_mp3, soundClipOptions );
