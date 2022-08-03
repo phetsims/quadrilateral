bfc46154b8eca8ff39e295619102476364f71006
Jesse <jesse.greenberg@colorado.edu>
2022-08-03 19:42:08 -0400

    add background tracks to the arpeggio, see https://github.com/phetsims/quadrilateral/issues/175

--- js/quadrilateral/view/sound/TracksArpeggioSoundView.ts
+++ js/quadrilateral/view/sound/TracksArpeggioSoundView.ts
@@ -27,10 +27,18 @@ import quadBuildingUpArpeggio004_mp3 from '../../../../sounds/quadBuildingUpArpe
 import quadBuildingUpArpeggio005_mp3 from '../../../../sounds/quadBuildingUpArpeggio005_mp3.js';
 import quadBuildingUpArpeggio006_mp3 from '../../../../sounds/quadBuildingUpArpeggio006_mp3.js';
 import quadBuildingUpArpeggio007_mp3 from '../../../../sounds/quadBuildingUpArpeggio007_mp3.js';
+import quadMelodyTracks000_mp3 from '../../../../sounds/quadMelodyTracks000_mp3.js';
+import quadMelodyTracks001_mp3 from '../../../../sounds/quadMelodyTracks001_mp3.js';
+import quadMelodyTracks002_mp3 from '../../../../sounds/quadMelodyTracks002_mp3.js';
+import quadMelodyTracks003_mp3 from '../../../../sounds/quadMelodyTracks003_mp3.js';
+import quadMelodyTracks004_mp3 from '../../../../sounds/quadMelodyTracks004_mp3.js';
+import quadMelodyTracks005_mp3 from '../../../../sounds/quadMelodyTracks005_mp3.js';
 
 // All the sounds played in this sound design
 // TODO: We are missing "optional" tracks in https://github.com/phetsims/quadrilateral/issues/175#issuecomment-1201643077
 const ARPEGGIO_TRACKS = [
+
+  // arpeggios
   quadBuildingUpArpeggio000_mp3,
   quadBuildingUpArpeggio001_mp3,
   quadBuildingUpArpeggio002_mp3,
@@ -38,7 +46,15 @@ const ARPEGGIO_TRACKS = [
   quadBuildingUpArpeggio004_mp3,
   quadBuildingUpArpeggio005_mp3,
   quadBuildingUpArpeggio006_mp3,
-  quadBuildingUpArpeggio007_mp3
+  quadBuildingUpArpeggio007_mp3,
+
+  // additional tracks behind the arpeggios
+  quadMelodyTracks000_mp3, // 8
+  quadMelodyTracks001_mp3, // 9
+  quadMelodyTracks002_mp3, // 10
+  quadMelodyTracks003_mp3, // 11
+  quadMelodyTracks004_mp3, // 12
+  quadMelodyTracks005_mp3 // 13
 ];
 
 // Each NamedQuadrilateral is assigned zero or more of the above tracks to play when the sim is in that state.
@@ -57,6 +73,23 @@ const NAMED_QUADRILATERAL_TO_TRACKS_MAP = new Map( [
   [ NamedQuadrilateral.SQUARE, [ 0, 1, 2, 3, 4, 5, 6, 7 ] ]
 ] );
 
+// In this sound design named quadrilaterals will also play a particular melody in the background
+// while the arpeggio plays. Keys are the named quadrilateral, indices are the index of ARPEGGIO_TRACKS
+// that should play for that named quadrilateral according to
+// https://github.com/phetsims/quadrilateral/issues/175#issuecomment-1204310364
+const NAMED_QUADRILATERAL_TO_BACKGROUND_TRACKS_MAP = new Map( [
+  [ NamedQuadrilateral.CONVEX_QUADRILATERAL, [] ],
+  [ NamedQuadrilateral.CONCAVE_QUADRILATERAL, [] ],
+  [ NamedQuadrilateral.DART, [ 8 ] ],
+  [ NamedQuadrilateral.KITE, [ 8 ] ],
+  [ NamedQuadrilateral.TRAPEZOID, [ 13 ] ],
+  [ NamedQuadrilateral.ISOSCELES_TRAPEZOID, [ 13 ] ],
+  [ NamedQuadrilateral.PARALLELOGRAM, [ 12 ] ],
+  [ NamedQuadrilateral.RHOMBUS, [ 11 ] ],
+  [ NamedQuadrilateral.RECTANGLE, [ 10 ] ],
+  [ NamedQuadrilateral.SQUARE, [ 9 ] ]
+] );
+
 class TracksArpeggioSoundView extends TracksSoundView {
   private readonly disposeTracksArpeggioSoundView: () => void;
 
@@ -70,6 +103,15 @@ class TracksArpeggioSoundView extends TracksSoundView {
         soundClip.setOutputLevel( 0 );
       } );
 
+      // play the background sound for the named quadrilateral - lower output level since it is in the
+      // background
+      const backgroundIndicesToPlay = NAMED_QUADRILATERAL_TO_BACKGROUND_TRACKS_MAP.get( shapeName );
+      assert && assert( backgroundIndicesToPlay, 'NamedQuadrilateral does not have a TracksArpeggioSoundView design' );
+      backgroundIndicesToPlay!.forEach( index => {
+        this.soundClips[ index ].setOutputLevel( 0.25 );
+      } );
+
+      // play the arpeggio tracks
       const soundIndicesToPlay = NAMED_QUADRILATERAL_TO_TRACKS_MAP.get( shapeName );
       assert && assert( soundIndicesToPlay, 'NamedQuadrilateral does not have a TracksArpeggioSoundView design' );
       soundIndicesToPlay!.forEach( index => {
