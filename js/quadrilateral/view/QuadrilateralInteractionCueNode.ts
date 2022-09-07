4bf556d72919fabe23c916631f3e998ef60130fc
Jesse <jesse.greenberg@colorado.edu>
2022-09-07 18:54:56 -0400

    update visual interaction cues, see #195

--- js/quadrilateral/view/QuadrilateralInteractionCueNode.ts
+++ js/quadrilateral/view/QuadrilateralInteractionCueNode.ts
@@ -10,17 +10,12 @@
 import { Path } from '../../../../scenery/js/imports.js';
 import ModelViewTransform2 from '../../../../phetcommon/js/view/ModelViewTransform2.js';
 import QuadrilateralColors from '../../common/QuadrilateralColors.js';
-import ArrowShape from '../../../../scenery-phet/js/ArrowShape.js';
 import QuadrilateralShapeModel from '../model/QuadrilateralShapeModel.js';
 import QuadrilateralConstants from '../../common/QuadrilateralConstants.js';
 import Side from '../model/Side.js';
 import { Line, Shape } from '../../../../kite/js/imports.js';
 import quadrilateral from '../../quadrilateral.js';
 import Emitter from '../../../../axon/js/Emitter.js';
-
-// These are in MODEL coordinates to control the length of the arrow guides
-const ARROW_DISTANCE_FROM_CENTER = 0.32;
-const ARROW_LENGTH = 0.2;
 
 const PATH_OPTIONS = {
   stroke: QuadrilateralColors.interactionCueColorProperty,
@@ -35,26 +30,33 @@ class QuadrilateralInteractionCueNode extends Path {
     // vertices
     quadrilateralShapeModel.vertices.forEach( vertex => {
       const viewBounds = modelViewTransform.modelToViewBounds( vertex.modelBoundsProperty.value ).dilate( QuadrilateralConstants.POINTER_AREA_DILATION );
-      nodeShape.rect( viewBounds.x, viewBounds.y, viewBounds.width, viewBounds.height );
+      nodeShape.circle( viewBounds.centerX, viewBounds.centerY, viewBounds.width / 2 );
+
+      // so that you don't have to include the start point for the circles
+      nodeShape.newSubpath();
     } );
 
     // sides
-    const tailWidth = modelViewTransform.modelToViewDeltaX( Side.SIDE_WIDTH );
+    const indicatorViewWidth = modelViewTransform.modelToViewDeltaX( Side.SIDE_WIDTH * 1.5 );
     quadrilateralShapeModel.sides.forEach( side => {
-      const midPoint = side.getMidpoint();
-      const midLine = new Line( midPoint, side.vertex2.positionProperty.value );
-      const tipPoint = midLine.startTangent.perpendicular.times( ARROW_DISTANCE_FROM_CENTER );
-      const tailPoint = midLine.startTangent.perpendicular.times( ARROW_DISTANCE_FROM_CENTER + ARROW_LENGTH );
 
-      const viewTipPoint = modelViewTransform.modelToViewPosition( tipPoint );
-      const viewTailPoint = modelViewTransform.modelToViewPosition( tailPoint );
+      // model coordinates
+      const midLine = new Line( side.vertex1.positionProperty.value, side.vertex2.positionProperty.value );
 
-      const arrowPoints = ArrowShape.getArrowShapePoints( viewTailPoint.x, viewTailPoint.y, viewTipPoint.x, viewTipPoint.y, [], {
-        tailWidth: tailWidth,
-        headWidth: tailWidth * 2,
-        headHeight: tailWidth
-      } );
-      nodeShape.polygon( arrowPoints );
+      // view coordinates
+      const tipViewPoint = modelViewTransform.modelToViewPosition( midLine.positionAt( 0.2 ) );
+      const tailViewPoint = modelViewTransform.modelToViewPosition( midLine.positionAt( 0.8 ) );
+      const reducedMidLine = new Line( tipViewPoint, tailViewPoint );
+
+      const leftStroke = reducedMidLine.strokeLeft( indicatorViewWidth )[ 0 ];
+      const rightStroke = reducedMidLine.strokeRight( indicatorViewWidth )[ 0 ];
+
+      // create a rectangle from the strokes along the midLine
+      nodeShape.moveToPoint( leftStroke.start );
+      nodeShape.lineToPoint( leftStroke.end );
+      nodeShape.lineToPoint( rightStroke.start );
+      nodeShape.lineToPoint( rightStroke.end );
+      nodeShape.close();
     } );
 
     super( nodeShape, PATH_OPTIONS );
