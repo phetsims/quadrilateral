f43517c553667007262103ce5ea7a7a01593fe25
Jesse <jesse.greenberg@colorado.edu>
2022-12-20 19:07:32 -0500

    much better drawing code, see https://github.com/phetsims/quadrilateral/issues/263

--- js/quadrilateral/view/QuadrilateralIconFactory.ts
+++ js/quadrilateral/view/QuadrilateralIconFactory.ts
@@ -4,15 +4,18 @@
  * @author Jesse Greenberg (PhET Interactive Simulations)
  */
 
-import { Circle, FlowBox, HBox, Line, Node, Path, Text } from '../../../../scenery/js/imports.js';
+import { Circle, FlowBox, HBox, Line, Node, Path, PathOptions, Text } from '../../../../scenery/js/imports.js';
 import Utils from '../../../../dot/js/Utils.js';
 import GridIcon from '../../../../scenery-phet/js/GridIcon.js';
 import quadrilateral from '../../quadrilateral.js';
 import QuadrilateralConstants from '../../common/QuadrilateralConstants.js';
 import QuadrilateralColors from '../../common/QuadrilateralColors.js';
-import { Shape } from '../../../../kite/js/imports.js';
+import { Line as ShapeLine, Shape } from '../../../../kite/js/imports.js';
 import musicSolidShape from '../../../../sherpa/js/fontawesome-5/musicSolidShape.js';
+import Vector2 from '../../../../dot/js/Vector2.js';
+import Ray2 from '../../../../dot/js/Ray2.js';
 import Matrix3 from '../../../../dot/js/Matrix3.js';
+import { combineOptions } from '../../../../phet-core/js/optionize.js';
 
 const QuadrilateralIconFactory = {
 
@@ -37,80 +40,103 @@ const QuadrilateralIconFactory = {
    */
   createMarkersIcon(): Node {
 
-    // The angle between sides in the icon, more than 90 degrees so it has more of a protractor look
-    const sideAngle = Utils.toRadians( 100 );
-    const sideLength = QuadrilateralIconFactory.ICON_HEIGHT;
-    const sideHeight = QuadrilateralIconFactory.ICON_HEIGHT * 0.25; // chosen by inspection
+    const pathOptions = {
+      stroke: QuadrilateralColors.visibilityIconsColorProperty,
+      lineWidth: QuadrilateralIconFactory.ICON_LINE_WIDTH
+    };
 
-    const bottomSide = Shape.rectangle( 0, 0, sideLength, sideHeight );
+    const sidesAngle = Utils.toRadians( 110 );
+    const sidesLength = QuadrilateralIconFactory.ICON_HEIGHT;
+    const sidesHeight = QuadrilateralIconFactory.ICON_HEIGHT * 0.25; // chosen by inspection
 
-    // Effectively rotating the bottom side about its bottom left corner by sideAngle
-    const verticalSide = bottomSide.transformed( Matrix3.translationRotation( 0, sideHeight, -sideAngle ) );
+    // draw the "sides"
+    const sidesShape = new Shape();
 
-    // use CAG to easily combine the sides into a single strokable shape without interior (overlapping) lines
-    const sidesShape = bottomSide.shapeUnion( verticalSide );
-    const sidesPath = new Path( sidesShape, { stroke: 'black', fill: QuadrilateralColors.screenBackgroundColorProperty, lineWidth: QuadrilateralIconFactory.ICON_LINE_WIDTH } );
+    // all drawing is relative to this origin
+    const origin = new Vector2( 0, 0 );
+
+    // bottom right of the horizontal side
+    const bottomRightPoint = new Vector2( sidesLength, sidesHeight );
+
+    // angle used to calculate new points perpendicular to others
+    const strokeLeftAngle = Math.PI / 2;
+
+    // the top right point of the vertical side
+    const leftSideTopPoint = new Vector2( Math.cos( -sidesAngle ) * sidesLength, Math.sin( -sidesAngle ) * sidesLength );
+
+    // the top left point of the vertical side
+    const strokedLeftSidePoint = leftSideTopPoint.plusXY( Math.cos( sidesAngle + strokeLeftAngle ) * sidesHeight, -Math.sin( sidesAngle + strokeLeftAngle ) * sidesHeight );
+
+    // the hardest part...we need to find the bottom point where the two outer lines of both sides intersect - we do
+    // this by creating a line/ray pair that runs along the outer lines and using Kite to find the intersection point
+    const bottomRay = new Ray2( bottomRightPoint, new Vector2( -1, 0 ) );
+    const leftSideLine = new ShapeLine(
+      strokedLeftSidePoint,
+
+      // Line rotated back around to the origin, then with extra length to find the intersection point
+      strokedLeftSidePoint.plusXY(
+        Math.cos( sidesAngle + 2 * strokeLeftAngle ) * sidesLength * 2,
+        -Math.sin( sidesAngle + 2 * strokeLeftAngle ) * sidesLength * 2 )
+    );
+    const intersections = leftSideLine.intersection( bottomRay );
+    assert && assert( intersections.length === 1, 'Needs to be one and only one intersection point' );
+    const bottomSideConnectionPoint = intersections[ 0 ].point;
+
+    // Now that we have all points, draw the sides
+    sidesShape.moveToPoint( origin );
+    sidesShape.lineTo( sidesLength, 0 );
+    sidesShape.lineTo( sidesLength, sidesHeight );
+    sidesShape.lineToPoint( bottomSideConnectionPoint );
+
+    // the outline of the vertical side
+    sidesShape.lineToPoint( strokedLeftSidePoint );
+    sidesShape.lineToPoint( leftSideTopPoint );
+
+    // close back to the origin
+    sidesShape.close();
+
+    const sidesPath = new Path( sidesShape, pathOptions );
+
+    // // creates two arcs that look like the perpendicular axis angle split into three 30 degree segments
+    const innerIconRadius = sidesLength * 0.4; // by inspection
+    const innerArcShape = Shape.arc( 0, 0, innerIconRadius, 0, 2 * Math.PI - sidesAngle, true );
+    const innerArcPath = new Path( innerArcShape, pathOptions );
+
+    const outerIconRadius = sidesLength * 0.8;
+    const outerArcShape = Shape.arc( 0, 0, outerIconRadius, 0, 2 * Math.PI - sidesAngle, true );
+    const outerArcPath = new Path( outerArcShape, pathOptions );
+
+    const sliceAngle = Math.PI / 6;
+    const numberOfSlices = Math.floor( sidesAngle / sliceAngle );
+
+    const halfRadius = ( outerIconRadius + innerIconRadius ) / 2;
+    const arcPaths = [];
+    for ( let i = 0; i < numberOfSlices; i = i + 2 ) {
+
+      const startAngle = i * sliceAngle;
+      const endAngle = startAngle + sliceAngle;
+      const arcShape = Shape.arc( 0, 0, halfRadius, 2 * Math.PI - startAngle, 2 * Math.PI - endAngle, true );
+      arcPaths.push( new Path( arcShape, combineOptions<PathOptions>( {}, pathOptions, { lineWidth: outerIconRadius - innerIconRadius } ) ) );
+    }
 
     // ticks (along the bottom side)
     const ticksShape = new Shape();
     for ( let i = 0; i < 3; i++ ) {
-      const tickX = 5 + i * sideLength / 3; // offset a little from the origin
-      ticksShape.moveTo( tickX, sideHeight );
-      ticksShape.lineTo( tickX, sideHeight / 2 );
+      const tickX = 5 + i * sidesLength / 3; // offset a little from the origin
+      ticksShape.moveTo( tickX, sidesHeight );
+      ticksShape.lineTo( tickX, sidesHeight / 2 );
     }
-    const bottomTicksPath = new Path( ticksShape, { stroke: 'black' } );
+    const bottomTicksPath = new Path( ticksShape, pathOptions );
 
-    // Effectively translates the ticksShape to the left and then rotates it clockwise (opposite direction of sideAngle)
-    // it lies on the left-most edge of the vertical side
-    const verticalTicksShape = ticksShape.transformed( Matrix3.rotationAround( Math.PI - sideAngle, 0, sideHeight ).timesMatrix( Matrix3.translation( -sideLength, 0 ) ) );
-    const verticalTicksPath = new Path( verticalTicksShape, { stroke: 'black' } );
-
-    // creates two arcs that look like the perpendicular axis angle split into three 30 degree segments
-    const innerIconRadius = sideLength / 1.7; // by inspection
-    const innerArcShape = Shape.arc( 0, sideHeight, innerIconRadius, 0, 2 * Math.PI - sideAngle, true );
-    const innerArcPath = new Path( innerArcShape, { stroke: QuadrilateralColors.visibilityIconsColorProperty, lineWidth: QuadrilateralIconFactory.ICON_LINE_WIDTH } );
-
-    const outerIconRadius = sideLength;
-    const outerArcShape = Shape.arc( 0, sideHeight, outerIconRadius, 0, 2 * Math.PI - sideAngle, true );
-    const outerArcPath = new Path( outerArcShape, { stroke: QuadrilateralColors.visibilityIconsColorProperty, lineWidth: QuadrilateralIconFactory.ICON_LINE_WIDTH } );
-
-    // Draws an arc shape (returning a Path) between start and end angle, representing the "angle guides"
-    const createAngleArcPath = ( startAngle: number, endAngle: number ) => {
-      const angleArcShape = new Shape();
-
-      // the bottom right part of the arc (starting point)
-      const innerStartX = Math.cos( startAngle ) * innerIconRadius;
-      const innerStartY = -Math.sin( startAngle ) * innerIconRadius + sideHeight;
-
-      // the top right part of the arc (line up to this point)
-      const outerLineToX = Math.cos( startAngle ) * outerIconRadius;
-      const outerLineToY = -Math.sin( startAngle ) * outerIconRadius + sideHeight;
-
-      // top left part of the arc shape (arc to this point)
-      const outerArcToX = Math.cos( endAngle ) * outerIconRadius;
-      const outerArcToY = -Math.sin( endAngle ) * outerIconRadius + sideHeight;
-
-      // bottom left point for the arc shape (line down to this point)
-      const innerLineToX = Math.cos( endAngle ) * innerIconRadius;
-      const innerLineToY = -Math.sin( endAngle ) * innerIconRadius + sideHeight;
-
-      angleArcShape.moveTo( innerStartX, innerStartY );
-      angleArcShape.lineTo( outerLineToX, outerLineToY );
-      angleArcShape.ellipticalArcTo( outerIconRadius, outerIconRadius, 0, false, false, outerArcToX, outerArcToY );
-      angleArcShape.lineTo( innerLineToX, innerLineToY );
-      angleArcShape.ellipticalArcTo( innerIconRadius, innerIconRadius, 0, false, true, innerStartX, innerStartY );
-
-      return new Path( angleArcShape, {
-        fill: QuadrilateralColors.visibilityIconsColorProperty,
-        stroke: 'black'
-      } );
-    };
-
-    const firstAngleArcPath = createAngleArcPath( 0, Math.PI / 5 );
-    const secondAngleArcPath = createAngleArcPath( Math.PI / 2 - Math.PI / 6, Math.PI / 2 );
+    // Effectively translates the ticksShape to the left and then rotates it clockwise (opposite direction of
+    // sidesAngle) so that it lies on the left-most edge of the vertical side.
+    const verticalTicksShape = ticksShape.transformed( Matrix3.rotationAround( Math.PI - sidesAngle, 0, 0 ).timesMatrix( Matrix3.translation( -sidesLength, 0 ) ) );
+    const verticalTicksPath = new Path( verticalTicksShape, pathOptions );
 
     return new Node( {
-      children: [ innerArcPath, outerArcPath, firstAngleArcPath, secondAngleArcPath, sidesPath, bottomTicksPath, verticalTicksPath ],
+      children: [ sidesPath, innerArcPath, outerArcPath, ...arcPaths, bottomTicksPath, verticalTicksPath ],
+
+      // drawing relative to the origin means that the icon is wider than it should be
       maxWidth: QuadrilateralIconFactory.ICON_HEIGHT
     } );
   },
