1a1cb6473b43a988bddf255bbd51f79d89ef219d
Jesse <jesse.greenberg@colorado.edu>
2023-01-05 23:34:39 -0500

    Proposal for https://github.com/phetsims/quadrilateral/issues/298, see #298

--- js/quadrilateral/view/MinorIntervalsToggleSwitch.ts
+++ js/quadrilateral/view/MinorIntervalsToggleSwitch.ts
@@ -7,25 +7,43 @@
  */
 
 import Property from '../../../../axon/js/Property.js';
-import ToggleSwitch from '../../../../sun/js/ToggleSwitch.js';
 import quadrilateral from '../../quadrilateral.js';
-import { Text } from '../../../../scenery/js/imports.js';
-import Dimension2 from '../../../../dot/js/Dimension2.js';
-import QuadrilateralConstants from '../../common/QuadrilateralConstants.js';
+import LockNode from '../../../../scenery-phet/js/LockNode.js';
+import Checkbox from '../../../../sun/js/Checkbox.js';
+import QuadrilateralIconFactory from './QuadrilateralIconFactory.js';
 
-class MinorIntervalsToggleSwitch extends ToggleSwitch<boolean> {
+class MinorIntervalsToggleSwitch extends Checkbox {
   public constructor( lockToMinorIntervalsProperty: Property<boolean> ) {
-    super( lockToMinorIntervalsProperty, false, true, {
-      size: new Dimension2( 36, 18 )
+
+    const lockIcon = QuadrilateralIconFactory.createLabelledIcon(
+      new LockNode( lockToMinorIntervalsProperty, { scale: 0.4 } ),
+      'Small Steps'
+    );
+    super( lockToMinorIntervalsProperty, lockIcon, {
+      spacing: 10,
+
+      // TODO: Proposing these options, if design likes them lets refactor and do it right.
+      // See https://github.com/phetsims/quadrilateral/issues/298.
+      labelTagName: 'label',
+      labelContent: 'Small Steps',
+
+      voicingNameResponse: 'Small Steps',
+      voicingHintResponse: 'When locked, move corners and sides in small steps with or without visual grid.',
+
+      voicingCheckedObjectResponse: 'Locked',
+      voicingUncheckedObjectResponse: 'Unlocked',
+
+      checkedContextResponse: 'Corner and side movement locked to small steps.',
+      uncheckedContextResponse: 'Small steps no longer locked. Add Shift key to make small steps.'
     } );
 
     // TODO: What is the name for this component? See ub.com/phetsims/quadrilateral/issues/298
-    const labelNode = new Text( 'Lock to small grid', {
-      leftBottom: this.leftTop.minusXY( 0, 5 ),
-      font: QuadrilateralConstants.SCREEN_TEXT_FONT,
-      maxWidth: 250
-    } );
-    this.addChild( labelNode );
+    // const labelNode = new Text( 'Small Steps', {
+    //   leftBottom: this.leftTop.minusXY( 0, 5 ),
+    //   font: QuadrilateralConstants.SCREEN_TEXT_FONT,
+    //   maxWidth: 250
+    // } );
+    // this.addChild( labelNode );
   }
 }
 
