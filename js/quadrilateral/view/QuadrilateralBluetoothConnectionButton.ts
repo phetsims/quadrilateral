48017a7d163c1d909c77278ddb564d29941dcc8a
phet-dev <phetadmin@gmail.com>
2023-01-20 04:39:08 -0700

    update copyright dates from daily grunt work

--- js/quadrilateral/view/QuadrilateralBluetoothConnectionButton.ts
+++ js/quadrilateral/view/QuadrilateralBluetoothConnectionButton.ts
@@ -1,4 +1,4 @@
-// Copyright 2022, University of Colorado Boulder
+// Copyright 2022-2023, University of Colorado Boulder
 
 /**
  * To test connecting to a bluetooth device using web bluetooth. Note this uses Promises (as the
--- js/quadrilateral/view/sound/TracksVolumeEmphasisSoundView.ts
+++ js/quadrilateral/view/sound/TracksVolumeEmphasisSoundView.ts
@@ -1,4 +1,4 @@
-// Copyright 2022, University of Colorado Boulder
+// Copyright 2022-2023, University of Colorado Boulder
 
 /**
  * One of the "Tracks" sound designs. In this design, each track is always playing. Depending on the named shape
