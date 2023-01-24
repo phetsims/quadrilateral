78a38baa1059cad4b85bfdfdbd1b7dc21f7a9286
phet-dev <phetadmin@gmail.com>
2023-01-24 04:51:39 -0700

    update copyright dates from daily grunt work

--- js/quadrilateral/view/sound/TracksBuildUpSoundView.ts
+++ js/quadrilateral/view/sound/TracksBuildUpSoundView.ts
@@ -1,4 +1,4 @@
-// Copyright 2022, University of Colorado Boulder
+// Copyright 2022-2023, University of Colorado Boulder
 
 /**
  * One of the "Tracks" sound designs. In this design, each track is assigned to a specific attribute of the shape
