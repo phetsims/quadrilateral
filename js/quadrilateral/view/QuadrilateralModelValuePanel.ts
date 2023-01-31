82411eb4f4a61121c7b36237c5bc61fe50862410
phet-dev <phetadmin@gmail.com>
2023-01-31 04:46:10 -0700

    update copyright dates from daily grunt work

--- js/quadrilateral/view/QuadrilateralModelValuePanel.ts
+++ js/quadrilateral/view/QuadrilateralModelValuePanel.ts
@@ -1,4 +1,4 @@
-// Copyright 2022, University of Colorado Boulder
+// Copyright 2022-2023, University of Colorado Boulder
 
 /**
  * A panel that displays model values for debugging and as a temporary display while
