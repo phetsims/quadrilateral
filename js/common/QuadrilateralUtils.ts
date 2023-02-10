0a7fab8d92af67acc3026bcbd2acadc7103a827e
Jesse <jesse.greenberg@colorado.edu>
2023-02-10 17:14:12 -0500

    empty QuadrilateralUtils before moving, see #362

--- js/common/QuadrilateralUtils.ts
+++ js/common/QuadrilateralUtils.ts
@@ -4,19 +4,9 @@
  * @author Jesse Greenberg (PhET Interactive Simulations)
  */
 
-import dotRandom from '../../../dot/js/dotRandom.js';
 import quadrilateral from '../quadrilateral.js';
 
-const QuadrilateralUtils = {
-
-  /**
-   * Returns a random color for debugging. Color is semi-transparent as we want to see the rest of the sim and possibly
-   * other debugging colors under this.
-   */
-  getRandomColor(): string {
-    return `rgba(${dotRandom.nextInt( 255 )},${dotRandom.nextInt( 255 )},${dotRandom.nextInt( 255 )},0.5)`;
-  }
-};
+const QuadrilateralUtils = {};
 
 quadrilateral.register( 'QuadrilateralUtils', QuadrilateralUtils );
 export default QuadrilateralUtils;
