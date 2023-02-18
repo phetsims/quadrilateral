8e8ee43ec224f5a2adf771a23b72d682e25bb101
phet-dev <phetadmin@gmail.com>
2023-02-18 03:57:08 -0700

    update copyright dates from daily grunt work

--- js/quadrilateral/model/ShapeSnapshot.ts
+++ js/quadrilateral/model/ShapeSnapshot.ts
@@ -1,4 +1,4 @@
-// Copyright 2021-2022, University of Colorado Boulder
+// Copyright 2021-2023, University of Colorado Boulder
 
 /**
  * A snapshot of the model Properties at a point in time needed to compare against other states to watch
