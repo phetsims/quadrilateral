6881901793e8fb3486bb8cf1200edbf976329979
Jesse <jesse.greenberg@colorado.edu>
2023-02-26 14:00:34 -0500

    final documentation for serial connection prototype, see #396

--- js/quadrilateral/view/prototype/QuadrilateralSerialMessageSender.ts
+++ js/quadrilateral/view/prototype/QuadrilateralSerialMessageSender.ts
@@ -1,10 +1,21 @@
 // Copyright 2023, University of Colorado Boulder
 
 /**
- * A class that will send a representation of the model to a parent iframe. This is used in a
- * "p5 serial connection" prototype.
+ * A class that will send a representation of the model to a parent iframe that will eventually send values
+ * to a tangible device over serial connection.
  *
- * TODO: More documentation about this Prototype, how it works, and what is expected.
+ * This prototype was quickly thrown together based on a Medium tutorial at
+ * https://medium.com/@yyyyyyyuan/tutorial-serial-communication-with-arduino-and-p5-js-cd39b3ac10ce
+ *
+ * Basically it works by
+ * 1) The sim runs in an iframe in a parent p5.js wrapper.
+ * 2) A button in the sim sends values representing the shape of the quadrilateral to parent window with `postMessage`.
+ * 2) The wrapper uses p5 modules to send values to another app, which forwards to an arduino over serial communication.
+ * 3) The arduino receives the values and begins to actuate to physically reproduce the quadrilateral shape.
+ *
+ * Exploration happened in https://github.com/phetsims/quadrilateral/issues/341.
+ *
+ * @author Jesse Greenberg (PhET Interactive Simulations)
  */
 
 import Utils from '../../../../../dot/js/Utils.js';
@@ -24,6 +35,8 @@ class QuadrilateralSerialMessageSender {
    * the data to an actuated device with a serial connection.
    *
    * '(topLength,rightLength,bottomLength,leftLength,topLeftAngle,bottomRightAngle)'
+   *
+   * The device then parses this string and sets values accordingly.
    */
   public sendModelValuesString(): void {
     const topLength = this.formatValue( this.shapeModel.sideAB.lengthProperty.value );
@@ -42,7 +55,7 @@ class QuadrilateralSerialMessageSender {
 
   /**
    * Limits a model value to two decimal places to send a smaller string to the wrapper (and eventually to
-   * the actuated tangible device). I don't know if this is necessary but it seems reasonable.
+   * the actuated tangible device). I don't know if this is necessary, but it seems reasonable.
    */
   private formatValue( value: number ): number {
     return Utils.toFixedNumber( value, 2 );
