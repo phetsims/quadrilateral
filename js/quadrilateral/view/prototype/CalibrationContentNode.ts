a247a0343c5f1f946ef91aecc3a393abfea415f1
Jesse <jesse.greenberg@colorado.edu>
2023-02-26 13:31:57 -0500

    final cleanup for CalibrationContentNode, see #396

--- js/quadrilateral/view/prototype/CalibrationContentNode.ts
+++ js/quadrilateral/view/prototype/CalibrationContentNode.ts
@@ -3,7 +3,7 @@
 /**
  * The contents of a Dialog that will support calibrating a device to the simulation. The device is feeding
  * physical values measured by sensors. Those need to be mapped to model coordinates in the sim. We calibrate
- * by asking the user to set their device as large as they can make it. From the largest lengths we can
+ * by asking the user to set their device as large as they can make it. From the largest device values, we can
  * create a linear mapping to model coordinates.
  *
  * @author Jesse Greenberg (PhET Interactive Simulations)
@@ -17,6 +17,9 @@ import optionize, { EmptySelfOptions } from '../../../../../phet-core/js/optioni
 import QuadrilateralConstants from '../../../QuadrilateralConstants.js';
 import TangibleConnectionModel from '../../model/prototype/TangibleConnectionModel.js';
 
+// not translatable because this is a prototype feature.
+const calibrationHintText = 'Extend your connected device to the maximum limits and then close this dialog box.';
+
 // We are going to draw circles representing the vertex positions in the dialog, this is their shared radius.
 const VERTEX_VIEW_RADIUS = 5;
 
@@ -27,7 +30,7 @@ class CalibrationContentNode extends VBox {
       align: 'center'
     }, providedOptions );
 
-    const calibrateHintText = new Text( 'Extend your connected device to the maximum limits and then close this dialog box.', QuadrilateralConstants.SCREEN_TEXT_OPTIONS );
+    const calibrateHintText = new Text( calibrationHintText, QuadrilateralConstants.SCREEN_TEXT_OPTIONS );
 
     // create a square shape to display the values provided by the quadrilateral model
     const viewBounds = new Bounds2( 0, 0, 300, 300 );
@@ -73,6 +76,7 @@ class CalibrationContentNode extends VBox {
     ];
     super( options );
 
+    // display device value as sides change
     tangibleConnectionModel.physicalModelBoundsProperty.link( physicalModelBounds => {
       if ( physicalModelBounds !== null ) {
         leftSideLengthText.text = Utils.toFixed( physicalModelBounds.height, 2 );
