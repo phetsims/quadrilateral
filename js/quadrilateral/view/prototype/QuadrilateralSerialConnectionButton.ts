590939b6d1bd2115518e303b9aa995e7027248c2
Jesse <jesse.greenberg@colorado.edu>
2023-02-26 13:47:48 -0500

    documentation to the top, see #396

--- js/quadrilateral/view/prototype/QuadrilateralSerialConnectionButton.ts
+++ js/quadrilateral/view/prototype/QuadrilateralSerialConnectionButton.ts
@@ -1,19 +1,20 @@
 // Copyright 2023, University of Colorado Boulder
 
+/**
+ * A button that will send values representing the model to a parent frame for the "p5 serial connection"
+ * prototype.
+ *
+ * See QuadrilateralSerialMessageController for more information about this prototype and how it works.
+ *
+ * @author Jesse Greenberg (PhET Interactive Simulations)
+ */
+
 import TextPushButton from '../../../../../sun/js/buttons/TextPushButton.js';
 import QuadrilateralColors from '../../../QuadrilateralColors.js';
 import QuadrilateralConstants from '../../../QuadrilateralConstants.js';
 import quadrilateral from '../../../quadrilateral.js';
 import TangibleConnectionModel from '../../model/prototype/TangibleConnectionModel.js';
 import QuadrilateralSerialMessageController from './QuadrilateralSerialMessageController.js';
-
-/**
- * A button that will send values representing the model to a parent frame for the "p5 serial connection"
- * prototype.
- *
- * TODO: More documentation for this prototype please.
- * @author Jesse Greenberg (PhET Interactive Simulations)
- */
 
 class QuadrilateralSerialConnectionButton extends TextPushButton {
   public constructor( tangibleConnectionModel: TangibleConnectionModel ) {
