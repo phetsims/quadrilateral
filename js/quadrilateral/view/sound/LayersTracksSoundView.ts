10cb84ffa7aa1b603f7032cd608a1a41c0e9ac68
Jesse <jesse.greenberg@colorado.edu>
2023-02-26 15:58:04 -0500

    documentation for the layers sound desing, see #346 and #396

--- js/quadrilateral/view/sound/LayersTracksSoundView.ts
+++ js/quadrilateral/view/sound/LayersTracksSoundView.ts
@@ -1,9 +1,10 @@
 // Copyright 2022-2023, University of Colorado Boulder
 
 /**
- * One of the "Tracks" sound designs. In this design, each track is assigned to a specific attribute of the shape
- * but builds toward more complex sounds as you reach a square. The square has the most pedagogically interesting
- * attributes of a quadrilateral even though it is the most basic shape, so it has the most complex sound design.
+ * In this design, tracks are mapped to a shape name and a sub-set of tracks will play at one time. Geometric
+ * properties of the quadrilateral build-up in "layers" until the user has a square. As more geometric properties are
+ * found, the sounds get more complex. Square is the most complex as it has the highest number of geometric property
+ * requirements.
  *
  * See https://github.com/phetsims/quadrilateral/issues/175#issuecomment-1201643077 for more information about this
  * design.
@@ -27,7 +28,7 @@ import NamedQuadrilateral from '../../model/NamedQuadrilateral.js';
 import QuadrilateralSoundOptionsModel from '../../model/QuadrilateralSoundOptionsModel.js';
 import Multilink from '../../../../../axon/js/Multilink.js';
 
-// All the sounds played in this sound design in the "simple" case.
+// All the sounds played in this sound design.
 const LAYER_TRACKS = [
   quadBaseBeatSimpler_mp3,
   quadBaseBeatSimplerConcaveQuadrilateralJustRhythmV2_mp3,
@@ -39,9 +40,9 @@ const LAYER_TRACKS = [
   quadBeatTracksBuildingBuildingTracks005_mp3
 ];
 
-// Each NamedQuadrilateral is assigned zero or more of the above tracks to play when the sim is in that state.
-// Values of the map are indices of the sound files above that should play for each NamedQuadrilateral, according
-// to the design in https://github.com/phetsims/quadrilateral/issues/175
+// Each NamedQuadrilateral is assigned zero or more of the above tracks to play. Values of the map are indices of the
+// sound files in the above array that should play for each NamedQuadrilateral, according to the design in
+// https://github.com/phetsims/quadrilateral/issues/175
 const NAMED_QUADRILATERAL_TO_TRACKS_MAP = new Map( [
   [ NamedQuadrilateral.CONVEX_QUADRILATERAL, [ 0 ] ],
   [ NamedQuadrilateral.CONCAVE_QUADRILATERAL, [ 1 ] ],
@@ -62,7 +63,7 @@ class LayersTracksSoundView extends TracksSoundView {
   public constructor( shapeModel: QuadrilateralShapeModel, shapeSoundEnabledProperty: TReadOnlyProperty<boolean>, resetNotInProgressProperty: TReadOnlyProperty<boolean>, soundOptionsModel: QuadrilateralSoundOptionsModel ) {
     super( shapeModel, shapeSoundEnabledProperty, resetNotInProgressProperty, soundOptionsModel, LAYER_TRACKS );
 
-    // desired output levels for each sound (as requested by design, using manual edit of the gain)
+    // desired output levels for each sound (as requested by design, after manually editing the gain)
     // See https://github.com/phetsims/quadrilateral/issues/175#issuecomment-1339626942
     this.setIndexOutputLevel( 0, 0.5 );
     this.setIndexOutputLevel( 1, 0.5 );
@@ -75,7 +76,7 @@ class LayersTracksSoundView extends TracksSoundView {
 
     const shapeNameListener = ( shapeName: NamedQuadrilateral ) => {
 
-      // First set all output levels back to nothing before we start playing new sounds
+      // First set all output levels back to 0 before we start playing new sounds
       this.soundClips.forEach( soundClip => {
         soundClip.setOutputLevel( 0 );
       } );
