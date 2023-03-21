991fe3aeb0056e2714447ed092b2b1f6c0b028ed
Jesse <jesse.greenberg@colorado.edu>
2023-03-21 15:59:23 -0400

    refactor unit tests for QuadrilateralShapeDetector, see #404

--- js/quadrilateral/model/QuadrilateralShapeDetectorTests.ts
+++ js/quadrilateral/model/QuadrilateralShapeDetectorTests.ts
@@ -12,6 +12,7 @@ import QuadrilateralShapeDetector from './QuadrilateralShapeDetector.js';
 import QuadrilateralShapeModel from './QuadrilateralShapeModel.js';
 import NamedQuadrilateral from './NamedQuadrilateral.js';
 import Vector2 from '../../../../dot/js/Vector2.js';
+import VertexLabel from './VertexLabel.js';
 
 QUnit.module( 'BooleanProperty' );
 QUnit.test( 'QuadrilateralShapeDetector', assert => {
@@ -19,71 +20,95 @@ QUnit.test( 'QuadrilateralShapeDetector', assert => {
   // Our test shape, with dummy Properties for the constructor (parts that we don't care about for testing)
   const shapeModel = new QuadrilateralShapeModel( new BooleanProperty( true ), new NumberProperty( 1 ) );
 
+  const setPositionsToShape = ( aPosition: Vector2, bPosition: Vector2, cPosition: Vector2, dPosition: Vector2 ) => {
+    shapeModel.setVertexPositions( new Map( [
+      [ VertexLabel.VERTEX_A, aPosition ],
+      [ VertexLabel.VERTEX_B, bPosition ],
+      [ VertexLabel.VERTEX_C, cPosition ],
+      [ VertexLabel.VERTEX_D, dPosition ]
+    ] ) );
+  };
+
   // Create one of each shape to verify basic detection. Position values found with dev
   // tool window.printVertexPositions() - only available when running with ?dev
-  shapeModel.vertexA.positionProperty.value = new Vector2( -0.25, 0.25 );
-  shapeModel.vertexB.positionProperty.value = new Vector2( 0.25, 0.25 );
-  shapeModel.vertexC.positionProperty.value = new Vector2( 0.25, -0.25 );
-  shapeModel.vertexD.positionProperty.value = new Vector2( -0.25, -0.25 );
+  setPositionsToShape(
+    new Vector2( -0.25, 0.25 ),
+    new Vector2( 0.25, 0.25 ),
+    new Vector2( 0.25, -0.25 ),
+    new Vector2( -0.25, -0.25 )
+  );
   assert.ok( QuadrilateralShapeDetector.getShapeName( shapeModel ) === NamedQuadrilateral.SQUARE, 'should be a square' );
 
-  shapeModel.vertexA.positionProperty.value = new Vector2( -0.25, 0.75 );
-  shapeModel.vertexB.positionProperty.value = new Vector2( 0.25, 0.75 );
-  shapeModel.vertexC.positionProperty.value = new Vector2( 0.25, -0.75 );
-  shapeModel.vertexD.positionProperty.value = new Vector2( -0.25, -0.75 );
+  setPositionsToShape(
+    new Vector2( -0.25, 0.75 ),
+    new Vector2( 0.25, 0.75 ),
+    new Vector2( 0.25, -0.75 ),
+    new Vector2( -0.25, -0.75 )
+  );
   assert.ok( QuadrilateralShapeDetector.getShapeName( shapeModel ) === NamedQuadrilateral.RECTANGLE, 'should be a rectangle' );
 
-  shapeModel.vertexA.positionProperty.value = new Vector2( 0, 0.75 );
-  shapeModel.vertexB.positionProperty.value = new Vector2( 0.5, 0 );
-  shapeModel.vertexC.positionProperty.value = new Vector2( 0, -0.75 );
-  shapeModel.vertexD.positionProperty.value = new Vector2( -0.5, 0 );
+  setPositionsToShape(
+    new Vector2( 0, 0.75 ),
+    new Vector2( 0.5, 0 ),
+    new Vector2( 0, -0.75 ),
+    new Vector2( -0.5, 0 )
+  );
   assert.ok( QuadrilateralShapeDetector.getShapeName( shapeModel ) === NamedQuadrilateral.RHOMBUS, 'should be a rhombus' );
 
-  shapeModel.vertexA.positionProperty.value = new Vector2( 0, 1 );
-  shapeModel.vertexB.positionProperty.value = new Vector2( 1.5, 0 );
-  shapeModel.vertexC.positionProperty.value = new Vector2( 0, -1 );
-  shapeModel.vertexD.positionProperty.value = new Vector2( -0.5, 0 );
+  setPositionsToShape(
+    new Vector2( 0, 1 ),
+    new Vector2( 1.5, 0 ),
+    new Vector2( 0, -1 ),
+    new Vector2( -0.5, 0 ) );
   assert.ok( QuadrilateralShapeDetector.getShapeName( shapeModel ) === NamedQuadrilateral.KITE, 'should be a kite' );
 
-  shapeModel.vertexA.positionProperty.value = new Vector2( -0.5, 0.75 );
-  shapeModel.vertexB.positionProperty.value = new Vector2( 0.5, 0.75 );
-  shapeModel.vertexC.positionProperty.value = new Vector2( 0.25, -0.25 );
-  shapeModel.vertexD.positionProperty.value = new Vector2( -0.25, -0.25 );
+  setPositionsToShape(
+    new Vector2( -0.5, 0.75 ),
+    new Vector2( 0.5, 0.75 ),
+    new Vector2( 0.25, -0.25 ),
+    new Vector2( -0.25, -0.25 )
+  );
   assert.ok( QuadrilateralShapeDetector.getShapeName( shapeModel ) === NamedQuadrilateral.ISOSCELES_TRAPEZOID, 'should be an isoceles trapezoid' );
 
-  shapeModel.vertexA.positionProperty.value = new Vector2( -0.5, 0.75 );
-  shapeModel.vertexB.positionProperty.value = new Vector2( 0.25, 0.75 );
-  shapeModel.vertexC.positionProperty.value = new Vector2( 0.25, -0.25 );
-  shapeModel.vertexD.positionProperty.value = new Vector2( -0.25, -0.25 );
+  setPositionsToShape(
+    new Vector2( -0.5, 0.75 ),
+    new Vector2( 0.25, 0.75 ),
+    new Vector2( 0.25, -0.25 ),
+    new Vector2( -0.25, -0.25 ) );
   assert.ok( QuadrilateralShapeDetector.getShapeName( shapeModel ) === NamedQuadrilateral.TRAPEZOID, 'should be a trapezoid' );
 
-  shapeModel.vertexA.positionProperty.value = new Vector2( 0, 1 );
-  shapeModel.vertexB.positionProperty.value = new Vector2( 1.5, 0 );
-  shapeModel.vertexC.positionProperty.value = new Vector2( 0, -1 );
-  shapeModel.vertexD.positionProperty.value = new Vector2( 0.5, 0.5 );
+  setPositionsToShape(
+    new Vector2( 0, 1 ),
+    new Vector2( 1.5, 0 ),
+    new Vector2( 0, -1 ),
+    new Vector2( 0.5, 0.5 ) );
   assert.ok( QuadrilateralShapeDetector.getShapeName( shapeModel ) === NamedQuadrilateral.CONCAVE_QUADRILATERAL, 'should be a concave quadrilateral' );
 
-  shapeModel.vertexA.positionProperty.value = new Vector2( 0, 1 );
-  shapeModel.vertexB.positionProperty.value = new Vector2( 1.5, 0 );
-  shapeModel.vertexC.positionProperty.value = new Vector2( 0, -1 );
-  shapeModel.vertexD.positionProperty.value = new Vector2( -1.25, 1 );
+  setPositionsToShape(
+    new Vector2( 0, 1 ),
+    new Vector2( 1.5, 0 ),
+    new Vector2( 0, -1 ),
+    new Vector2( -1.25, 1 ) );
   assert.ok( QuadrilateralShapeDetector.getShapeName( shapeModel ) === NamedQuadrilateral.CONVEX_QUADRILATERAL, 'should be a convex quadrilateral' );
 
-  shapeModel.vertexA.positionProperty.value = new Vector2( -0.25, 0.25 );
-  shapeModel.vertexB.positionProperty.value = new Vector2( 0.25, 0.25 );
-  shapeModel.vertexC.positionProperty.value = new Vector2( 0.25, -0.25 );
-  shapeModel.vertexD.positionProperty.value = new Vector2( 0, 0 );
+  setPositionsToShape(
+    new Vector2( -0.25, 0.25 ),
+    new Vector2( 0.25, 0.25 ),
+    new Vector2( 0.25, -0.25 ),
+    new Vector2( 0, 0 ) );
   assert.ok( QuadrilateralShapeDetector.getShapeName( shapeModel ) === NamedQuadrilateral.TRIANGLE, 'should be a triangle' );
 
-  shapeModel.vertexA.positionProperty.value = new Vector2( -1, 0.75 );
-  shapeModel.vertexB.positionProperty.value = new Vector2( -0.5, 0.75 );
-  shapeModel.vertexC.positionProperty.value = new Vector2( 0.75, -0.75 );
-  shapeModel.vertexD.positionProperty.value = new Vector2( 0.25, -0.75 );
+  setPositionsToShape(
+    new Vector2( -1, 0.75 ),
+    new Vector2( -0.5, 0.75 ),
+    new Vector2( 0.75, -0.75 ),
+    new Vector2( 0.25, -0.75 ) );
   assert.ok( QuadrilateralShapeDetector.getShapeName( shapeModel ) === NamedQuadrilateral.PARALLELOGRAM, 'should be a parallelogram' );
 
-  shapeModel.vertexA.positionProperty.value = new Vector2( -0.75, 0.75 );
-  shapeModel.vertexB.positionProperty.value = new Vector2( 1, 0.75 );
-  shapeModel.vertexC.positionProperty.value = new Vector2( 1, -1 );
-  shapeModel.vertexD.positionProperty.value = new Vector2( 0.5, 0.25 );
+  setPositionsToShape(
+    new Vector2( -0.75, 0.75 ),
+    new Vector2( 1, 0.75 ),
+    new Vector2( 1, -1 ),
+    new Vector2( 0.5, 0.25 ) );
   assert.ok( QuadrilateralShapeDetector.getShapeName( shapeModel ) === NamedQuadrilateral.DART, 'should be a dart' );
 } );
