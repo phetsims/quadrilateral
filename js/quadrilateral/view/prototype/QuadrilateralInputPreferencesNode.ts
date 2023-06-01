b5b58199b91d1d233ce0f66c1cb6fb7ac7a84310
Jesse <jesse.greenberg@colorado.edu>
2023-06-01 17:33:50 -0400

    use custom content for the MediaPipe options, see https://github.com/phetsims/quadrilateral/issues/258

--- js/quadrilateral/view/prototype/QuadrilateralInputPreferencesNode.ts
+++ js/quadrilateral/view/prototype/QuadrilateralInputPreferencesNode.ts
@@ -17,6 +17,13 @@ import PreferencesDialog from '../../../../../joist/js/preferences/PreferencesDi
 import QuadrilateralTangibleOptionsModel from '../../model/prototype/QuadrilateralTangibleOptionsModel.js';
 import MediaPipe from '../../../../../tangible/js/mediaPipe/MediaPipe.js';
 
+// Strings for the media pipe options content - this is a prototype and not available through production yet so
+// it is not translatable.
+const featureDescriptionString = 'Use custom hand gestures and movements to control objects in the sim. Please see the Teacher Tips for specific gestures, movements, and object mappings.';
+const troubleshootingDescriptionString = 'If needed, use the options below to change the behavior of the sim objects to match your hand movements.';
+const flipYAxisDescriptionString = 'Are your hands mapped to the wrong object or group of objects?';
+const flipXAxisDescriptionString = 'Are your hands moving the sim objects in what seems like the wrong direction?';
+
 export default class QuadrilateralInputPreferencesNode extends PreferencesPanelSection {
   private readonly disposeQuadrilateralInputPreferencesNode: () => void;
 
@@ -32,7 +39,15 @@ export default class QuadrilateralInputPreferencesNode extends PreferencesPanelS
     let titleNode: Node | null = null;
 
     if ( tangibleOptionsModel.cameraInputHandsConnectedProperty.value ) {
-      const mediaPipeContent = MediaPipe.getMediaPipeOptionsNode();
+      const mediaPipeContent = MediaPipe.getMediaPipeOptionsNode( {
+        featureDescriptionString: featureDescriptionString,
+        troubleshootingDescriptionString: troubleshootingDescriptionString,
+        flipYAxisDescriptionString: flipYAxisDescriptionString,
+        flipXAxisDescriptionString: flipXAxisDescriptionString,
+
+        // Disable maxWidth and default lineWrap so this dialog looks good in English (not translatable)
+        labelTextOptions: { lineWrap: 550, maxWidth: null }
+      } );
       children = [ mediaPipeContent ];
       disposeContents = () => {
         mediaPipeContent.dispose();
