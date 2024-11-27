e8a3550cdf2b1c87417b51e5aab434cac319c150
phet-dev <phetadmin@gmail.com>
2024-11-27 03:37:43 -0700

    update copyright dates from daily grunt work

--- js/quadrilateral/view/QuadrilateralScreenView.ts
+++ js/quadrilateral/view/QuadrilateralScreenView.ts
@@ -1,4 +1,4 @@
-// Copyright 2021-2023, University of Colorado Boulder
+// Copyright 2021-2024, University of Colorado Boulder
 
 /**
  * The ScreenView for this simulation. For readability, implementation of this class is broken into blocks of code that
