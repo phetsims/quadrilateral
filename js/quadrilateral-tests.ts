836bd7f3d08cf2659c23a878104de7a230437878
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-11-27 15:22:41 -0700

    move browser files in chipper into a sub directory, https://github.com/phetsims/chipper/issues/1534

--- js/quadrilateral-tests.ts
+++ js/quadrilateral-tests.ts
@@ -6,7 +6,7 @@
  * @author Jesse Greenberg (PhET Interactive Simulations)
  */
 
-import qunitStart from '../../chipper/js/sim-tests/qunitStart.js';
+import qunitStart from '../../chipper/js/browser/sim-tests/qunitStart.js';
 import './quadrilateral/model/QuadrilateralShapeDetectorTests.js';
 
 qunitStart();
