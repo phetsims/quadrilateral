e6521a6a7d817bce0a05a49a7296ab444154e8a0
phet-dev <phetadmin@gmail.com>
2025-01-08 03:44:26 -0700

    update copyright dates from daily grunt work

--- js/quadrilateral/view/sound/QuadrilateralSoundOptionsNode.ts
+++ js/quadrilateral/view/sound/QuadrilateralSoundOptionsNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2021-2024, University of Colorado Boulder
+// Copyright 2021-2025, University of Colorado Boulder
 
 /**
  * Options for the Preferences Dialog that allow us to set the sound design and sub-options.
