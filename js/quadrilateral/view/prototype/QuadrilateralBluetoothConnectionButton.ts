1edeb28829632230ff137f7164c8b8df3c75769f
phet-dev <phetadmin@gmail.com>
2025-04-17 04:11:56 -0600

    update copyright dates from daily grunt work

--- js/quadrilateral/view/prototype/QuadrilateralBluetoothConnectionButton.ts
+++ js/quadrilateral/view/prototype/QuadrilateralBluetoothConnectionButton.ts
@@ -1,4 +1,4 @@
-// Copyright 2022-2024, University of Colorado Boulder
+// Copyright 2022-2025, University of Colorado Boulder
 
 /**
  * To test connecting to a bluetooth device using web bluetooth. This code was written in collaboration with Scott
