f4feb061076cd295ff9dda88c9a687588672218a
phet-dev <phetadmin@gmail.com>
2026-03-21 04:48:55 -0600

    update copyright dates from daily grunt work

--- js/quadrilateral/model/prototype/QuadrilateralTangibleOptionsModel.ts
+++ js/quadrilateral/model/prototype/QuadrilateralTangibleOptionsModel.ts
@@ -1,4 +1,4 @@
-// Copyright 2023-2024, University of Colorado Boulder
+// Copyright 2023-2026, University of Colorado Boulder
 
 /**
  * Model component for options related to control from the tangible. These Properties are controlled by UI components
--- js/quadrilateral/view/prototype/QuadrilateralTangibleController.ts
+++ js/quadrilateral/view/prototype/QuadrilateralTangibleController.ts
@@ -1,4 +1,4 @@
-// Copyright 2023-2024, University of Colorado Boulder
+// Copyright 2023-2026, University of Colorado Boulder
 
 /**
  * A general class that updates the sim from tangible device input. In that way it "controls" the simulation model.
